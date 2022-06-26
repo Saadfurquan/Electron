7728ea903f13dd49dd5205d82406ba78e9dc4f28
Saad Furquan <saadfurquan@gmail.com>
2022-06-26 22:41:39 +0530

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log('test');
+const electron =require("electron");
+const app =electron.app;
+const browserWindow=electron.browserWindow;
+const path=require("path");
+const url=require("url");
+let win;
+function createWindow(){
+    win = new browserWindow();
+    win.loadURL(url.format({pathname:path.join(__dirname,'index.html'),
+    protocol:'file',
+    slashes:true
+
+
+
+}));
+win.on('closed',()=>{win=null;})
+}
+app.on('ready',createWindow);
+
+
+
+
+
